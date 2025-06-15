e56a4e1fce6e35ed09172e949605fb23dc8a9054
Aryanparashara <aryansharma33687@gmail.com>
2025-06-15 23:45:29 +0530

    Create practice1.c

---
+++ practice1.c
@@ -0,0 +1,50 @@
+/*#include<stdio.h>
+void main ()
+{
+int a,b,c;
+printf("\n enter 2 no.s ");
+scanf("%d %d",&a,&b);
+c=a+b;
+printf("\n sum is %d",c);
+}*/
+/*#include <stdio.h>
+void main ()
+{
+int a,b,c,d,e,f;
+printf("\n enter marks of Hindi ");
+scanf("%d",&a);
+printf(" \n enter marks of english");
+scanf("%d",&b);
+printf("\n enter marks of maths ");
+scanf("%d",&c);
+printf("\n enter marks of science");
+scanf("%d",&d);
+printf("\n enter marks of arts");
+scanf("%d",&e);
+f=a+b+c+d+e;
+printf("\n sum is %d",f);
+}*/
+/*#include<stdio.h>
+void main ()
+{
+int p,r,t,si;
+printf("\n enter the principle amount");
+scanf("%d",&p);
+printf("\n enter the rate");
+scanf("%d",&r);
+printf("\n enter the time");
+scanf("%d",&t);
+si=(p*r*t)/100;
+printf("\n simple interest is %d ",si);
+}*/
+/*#include<stdio.h>
+void main ()
+{
+int r,h,voc;
+printf(" \n enter the radius");
+scanf("%d",&r);
+printf(" \n enter the height");
+scanf("%d",&h);
+voc=(3.14*r*r*h)/3;
+printf("\n volume of cone is %d",voc);
+}*/

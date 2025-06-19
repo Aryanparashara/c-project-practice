b37a53e9f620818892db5f764a83f4c8607b9540
Aryanparashara <aryanparashara01@gmail.com>
2025-06-19 23:00:54 +0530

    Create cbse marksheet+scholarship.c

---
+++ cbse marksheet+scholarship.c
@@ -0,0 +1,46 @@
+/*#include<stdio.h>
+int main()
+{
+  int a,b,c,d,e,f,g,per;
+  printf("\n CENTRAL BOARD OF SECONDARY EDUCATION");
+  printf("\n              2025-26              ");
+  printf("\n ____________________________________");
+  printf("\n   sub     sub      max      marks   ");
+  printf("\n   code    name    marks    obtained ");
+  printf("\n ____________________________________");
+  printf("\n   101    English   100      ");
+  scanf("%d",&a);
+  printf("\n   102     Math     100      ");
+  scanf("%d",&b);
+  printf("\n   103    Computer  100      ");
+  scanf("%d",&c);
+  printf("\n   104     Hindi    100      ");
+  scanf("%d",&d);
+  printf("\n   105    Science   100      ");
+  scanf("%d",&e);
+  printf("\n   106     Arts     100      ");
+  scanf("%d",&f);
+  g=a+b+c+d+e+f;
+  printf("\n ____________________________________");
+  printf("\n  Total Marks out of 600  -  %d",g);
+  printf("\n ____________________________________");
+  per=g*100/600;
+  printf("\n           percentage is %d          ",per);
+  printf("\n ____________________________________");
+  if (per>=90)
+  {
+  printf("\n 100per. of scholarship ");
+  }
+  else if ((per>=60)&&(per<90))
+  {
+    printf("\n 60per. of scholarship ");
+  }
+  else if ((per>=40)&&(per<60))
+  {
+    printf("\n 40per. of scholarship ");
+  }
+  else
+  {
+    printf("\n No scholarship");
+  }
+  }*/

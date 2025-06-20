804d3f4239f5bf611b31ff7a4bd0017b18db4ef1
Aryanparashara <aryanparashara01@gmail.com>
2025-06-20 22:54:58 +0530

    Create bill by if else .c

---
+++ bill by if else .c
@@ -0,0 +1,30 @@
+/*#include<stdio.h>
+int main ()
+{
+  int unit,bill,totalbill;
+  printf("\n enter the unit ");
+  scanf("%d",&unit);
+  printf("\n monthly charges will be \u20B9550");
+  if ((unit>=0)&&(unit<=100))
+  {
+  bill=(unit*5);
+  printf("\n bill will be \u20B9%d",bill);
+  }
+  else if ((unit>=100)&&(unit<=300))
+  {
+    bill=(unit*8);
+    printf("\n bill will be\u20B9%d",bill);
+  }
+  else if (unit>=300)
+  {
+    bill=(unit*11);
+   printf("\n bill will be \u20B9%d",bill);
+  }
+  else
+  {
+   bill=(unit*11);
+   printf("\n bill will be \u20B9%d",bill);
+  }
+  totalbill=bill+550;
+  printf("\n totalbill will be \u20B9%d",totalbill);
+}*/

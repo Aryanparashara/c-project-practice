28ac497c23643acfc3345d8a11950c46355e6cbd
Aryanparashara <aryanparashara01@gmail.com>
2025-06-21 22:39:10 +0530

    Update bill by if else .c

--- bill by if else .c
+++ bill by if else .c
@@ -1,29 +1,25 @@
 /*#include<stdio.h>
-int main()
+int main ()
 {
-int unit,bill,totalbill;
-printf("\n Enter the unit ");
-scanf("%d",&unit);
-printf("\n Monthly charges will be \u20B9550");
-if ((unit>=0)&&(unit<=100))
-{
+  int unit,bill,totalbill;
+  printf("\n enter the unit ");
+  scanf("%d",&unit);
+  printf("\n monthly charges will be \u20B9550");
+  if ((unit>=0)&&(unit<=100))
+  {
   bill=(unit*5);
-  printf("\n Bill will be \u20B9%d",bill);
-}
-else if ((unit>=100)&&(unit<=300))
-{
-  bill=(unit*8);
-  printf("\n Bill wuill be \u20B9%d",bill);
-}\
-else if (unit>=300)
-{
-  bill=(unit*11);
-  printf("\n Bill will be \u20B9%d",bill);
-}
-else
-{
-  printf("\n Bill will be \u20B9%d",bill);
-}
-totalbill=(bill+550);
-printf("\n Total bill will be \u20B9%d",totalbill);
+  printf("\n bill will be \u20B9%d",bill);
+  }
+  else if ((unit>=100)&&(unit<=300))
+  {
+    bill=(unit-100)*8+(100*5);
+    printf("\n bill will be\u20B9%d",bill);
+  }
+  else if (unit>=300)
+  {
+    bill=(unit-300)*11+(100*5)+(200*8);
+   printf("\n bill will be \u20B9%d",bill);
+  }
+  totalbill=bill+550;
+  printf("\n totalbill will be \u20B9%d",totalbill);
 }*/

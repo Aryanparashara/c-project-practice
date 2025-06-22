6b2ee267817bbba821d8ccabf8a3d743f5d57cd5
Aryanparashara <aryanparashara01@gmail.com>
2025-06-22 22:48:39 +0530

    Create square and cubeof a no.c

---
+++ square and cubeof a no.c
@@ -0,0 +1,11 @@
+/*#include<stdio.h>
+void main()
+{
+  int a,s,c;
+  printf("\n enter a no.");
+  scanf("%d",&a);
+  s=(a*a);
+  printf("\n square is %d",s);
+  c=(a*a*a);
+  printf("\n cube is %d",c);
+}*/

5ef35ecf7ed9c4d02fb87edcfbefb1a4945048f3
Aryanparashara <aryanparashara01@gmail.com>
2025-06-23 10:19:33 +0530

    Create temprature in celscius.c

---
+++ temprature in celscius.c
@@ -0,0 +1,9 @@
+/*#include<stdio.h>
+void main()
+{
+  int f,c;
+  printf("\n enter temprature in celsius:");
+  scanf("%d",&c);
+  f=(c*9/5)+32;
+  printf("\n temperature in fahrenhiet is %d",f);
+}*/

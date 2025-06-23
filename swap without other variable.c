fc0d459bec98dff16023c0a90a4bba097d70e702
Aryanparashara <aryanparashara01@gmail.com>
2025-06-23 10:17:26 +0530

    Create swap without other variable.c

---
+++ swap without other variable.c
@@ -0,0 +1,15 @@
+/*#include<stdio.h>
+void main()
+{
+  int a,b;
+  printf("\n enter first no.(a)");
+  scanf("%d",&a);
+  printf("\n enter second no.(b)");
+  scanf("%d",&b);
+  a=a+b;
+  b=a-b;
+  a=a-b;
+  printf("\n after swapping no.s are");
+  printf("\n a is %d",a);
+  printf("\n b is %d',b);
+    }*/

6d048aa80647d9f890a687939189b5f91b3d2cee
Aryanparashara <aryanparashara01@gmail.com>
2025-06-25 22:17:58 +0530

    Create quotient,remainder.c

---
+++ quotient,remainder.c
@@ -0,0 +1,13 @@
+/*#include<stdio.h>
+void main()
+{
+  int dividend,divisor,quotient,remainder;
+  printf("\n enter the dividend");
+  scanf("%d",&dividend);
+  printf("\n enter the divisor");
+  scanf("%d",&divisor);
+  quotient=dividend/divisor;
+  remainder=dividend%divisor;
+  printf("\n quotient is %d",quotient);
+  printf("\n remainder is %d",remainder);
+}*/

8b3d07fcf7de66177bb49e408f8e7867787188f6
Aryanparashara <aryanparashara01@gmail.com>
2025-06-26 11:59:39 +0530

    Create  find +,-,0 .c

---
+++ find +,-,0 .c
@@ -0,0 +1,13 @@
+ /*#include<stdio.h>
+int main() 
+{
+  int num;
+printf("enter a number: ");
+scanf("%d",&num);
+  if (num > 0) 
+printf("\n the number is positive");
+ else if (num < 0) 
+printf("\n the number is negative");
+ else 
+printf("\n the number is zero");
+}*/

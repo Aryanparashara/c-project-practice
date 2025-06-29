2d979d96414fcc7e50195820f7e6c5759feca09c
Aryanparashara <aryanparashara01@gmail.com>
2025-06-29 12:57:11 +0530

    Create circumference and area of circle .c

---
+++ circumference and area of circle .c
@@ -0,0 +1,11 @@
+/*#include<stdio.h>
+int main()
+{
+  int a,c,r;
+  printf("\n enter the radius");
+  scanf("%d",&r);
+  c=(2*3.14*r);
+  printf("\n circumference is %d",c);
+  a=(3.14*r*r);
+  printf("\n area is %d",a);
+}*/

e10cca573b8e93659422ff5ab4761a7dc6052b47
Aryanparashara <aryanparashara01@gmail.com>
2025-06-30 13:00:30 +0530

    Create factorial of a no.c

---
+++ factorial of a no.c
@@ -0,0 +1,14 @@
+/*#include<stdio.h>
+int main()
+{
+  unsigned long long factorial = 1; 
+  int i,num;
+  printf(" enter a positive integer");
+  scanf("%d",&num);
+  if (num<0)
+  printf("\n factorial of negative no. doesn't exist");
+  else
+  for(i=1;i<=num; i++)
+  factorial*=i;
+  printf("\n factorial of %d is %llu",num,factorial);
+}*/

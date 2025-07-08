3f1b38d6a1393a29acf3d75547b33fd0ba6a617e
Aryanparashara <aryanparashara01@gmail.com>
2025-07-08 22:14:51 +0530

    Create postfix and prefix and typecasting.c

---
+++ postfix and prefix and typecasting.c
@@ -0,0 +1,29 @@
+/*#include<stdio.h>
+int main ()
+{
+  int a=5,b=5;
+  // use postfix mode for a and prefix mode for b
+  printf("\n %8d%10d",a--,--b);
+  printf("\n %8d%10d",a--,--b);
+  printf("\n %8d%10d",a--,--b);
+  printf("\n %8d%10d",a--,--b);
+  printf("\n %8d%10d",a--,--b);
+}*/
+
+ // Without Type casting.
+/*#include<stdio.h>
+int main()
+{
+  int a;
+  a=15/6;
+  printf("%d",a);
+}*/   
+
+// After Type casting.
+/*#include<stdio.h>
+int main()
+{
+  float a;
+  a=(float)15/6;
+  printf("%f",a);
+}*/

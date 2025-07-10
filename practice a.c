b02dca741359cd6cb69133b0fa83c100678b77b5
Aryanparashara <aryanparashara01@gmail.com>
2025-07-10 21:16:50 +0530

    Create practice a.c

---
+++ practice a.c
@@ -0,0 +1,47 @@
+/*#include<stdio.h>
+#include<stdlib.h>
+int main ()
+{
+  int ch,a,b,c,d,e,f;
+  printf("\n Enter 1. sum ");
+  printf("\n Enter 2. sub ");
+  printf("\n Enter 3. product ");
+  printf("\n Enter 4. division ");
+  printf("\n Enter 5. Exit ");
+  printf("\n Enter your choice ");
+  scanf("%d",&ch );
+  switch(ch)
+  {
+    case 1:
+    printf("\n Enter two no.s ");
+    scanf("%d%d",&a,&b);
+    c=a+b;
+    printf("\n sum is %d",c);
+    break;
+    case 2:
+    printf("\n Enter two no.s ");
+    scanf("%d%d",&a,&b);
+    d=a-b;
+    printf("\n sub is %d",d);
+    break;
+    case 3:
+    printf("\n Enter two no.s ");
+    scanf("%d%d",&a,&b);
+    e=a*b;
+    printf("\n product is %d",e);
+    break;
+    case 4:
+    printf("\n Enter two no.s ");
+    scanf("%d%d",&a,&b);
+    f=a/b;
+    printf("\n division is %d",f);
+    break;
+    case 5:
+    exit(0);
+    break;
+    default:
+    printf("\n INVALID CHOICE ");
+    printf("\n Enter 1,2,3,4,5 ");
+    break;
+  }
+}*/

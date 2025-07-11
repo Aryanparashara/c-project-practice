ba3ac6a9867a7036de5226cdb77602020dbb7164
Aryanparashara <aryanparashara01@gmail.com>
2025-07-11 21:51:10 +0530

    Create example of break statement.c

---
+++ example of break statement.c
@@ -0,0 +1,16 @@
+// example of break statement
+
+/*#include<stdio.h>
+int main()
+{
+  int i;
+  for(i=1;i<=10;i++)
+  {
+    printf("%d",i);
+    if(i==5)
+    {
+    break;
+    }
+  }
+  printf(" came outside of loop i=%d",i);
+}*/

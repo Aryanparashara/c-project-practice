1587969603c94acfc8d59c74f875699eb123fa06
Aryanparashara <aryanparashara01@gmail.com>
2025-07-11 10:04:23 +0530

    Create practice b.c

---
+++ practice b.c
@@ -0,0 +1,61 @@
+/*#include<stdio.h>
+int main()
+{
+int a,b,c,d,e,f; 
+char ch;
+printf("\n 1 or A or a or + Addittion ");
+printf("\n 2 or S or s or - subtraction ");
+printf("\n 3 or P or p or * product ");
+printf("\n 4 or D or d or / Division ");
+printf("\n 5 or E or e or . Exit ");
+printf("\n Enter your choice ");
+scanf("%c",&ch);
+switch(ch)
+{
+  case '1':
+  case 'A':
+  case 'a':
+  case '+':
+  printf("\n Enter two no.s ");
+  scanf("%d%d",&a,&b);
+  c=a+b;
+  printf("\n sum is %d",c);
+  break;
+  case '2':
+  case 'S':
+  case 's':
+  case '-':
+  printf("\n Enter two no.s ");
+  scanf("%d%d",&a,&b);
+  d=a-b;
+  printf("\n sub is %d",d);
+  break;
+  case '3':
+  case 'P':
+  case 'p':
+  case '*':
+  printf("\n Enter two no.s ");
+  scanf("%d%d",&a,&b);
+  e=a*b;
+  printf("\n product is %d",e);
+  break;
+  case '4':
+  case 'D':
+  case 'd':
+  case '/':
+  printf("\n Enter two no.s ");
+  scanf("%d%d",&a,&b);
+  f=a/b;
+  printf("\n division is %d",f);
+  break;
+  case '5':
+  case 'E':
+  case 'e':
+  case '.':
+  exit(0);
+  break;
+  default:
+  printf("\n Invalid choice ");
+  break;
+}
+}*/

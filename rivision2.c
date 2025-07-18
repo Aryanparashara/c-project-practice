a4685d318cfc59e1cb1083fe43b7048ff7778380
Aryanparashara <aryanparashara01@gmail.com>
2025-07-18 12:55:40 +0530

    Create rivision2.c

---
+++ rivision2.c
@@ -0,0 +1,46 @@
+/*#include<stdio.h>
+int main()
+{
+  int si,p,r,t;
+  printf("\n Enter the principle amount: ");
+  scanf("%d",&P);
+  printf("\n Enter the radius: ");
+  scanf("%d",&r);
+  printf("\n Enter the time: ");
+  scanf("%d",&t);
+  si=(p*r*t)/100;
+  printf("\n simple interest is %d",si);
+}*/
+
+/*#include<stdio.h>
+int main()
+{
+  int voc,r,h;
+  printf("\n enter the radius ");
+  scanf("%d",&r);
+  printf("\n enter the height ");
+  scanf("%d",&h);
+  voc=(3.14*r*r*h)/3;
+  printf("\n volume of cone is %d",voc);
+}*/
+
+/*#include<stdio.h>
+int main()
+{
+  int tada,hra,pf,medical,basic,net,gross;
+  printf("\n enter the basicsalary: ");
+  scanf("%d",&basic);
+  tada=(basic*30)/100;
+  printf("\n tada is %d",&tada);
+  hra=(basic*50)/100;
+  printf("\n hra is %d",&hra);
+  medical=(basic*20)/100;
+  printf("\n medical is %d",&medical);
+  pf=(basic*15)/100;
+  printf("\n pf is %d",&pf);
+  net=basic+tada+hra+medical+pf;
+  printf("\n net salary is %d",net);
+  gross=net-pf;
+  printf("\n gross salary is %d",gross);
+}*/
+

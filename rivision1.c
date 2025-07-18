fd7c3adab4fccb449a80b45e5aa745861034f983
Aryanparashara <aryanparashara01@gmail.com>
2025-07-18 11:49:14 +0530

    Create rivision1.c

---
+++ rivision1.c
@@ -0,0 +1,43 @@
+/*#include<stdio.h>
+int main()
+{
+  int a,b,c;
+  printf("\n Enter two no.s: ");
+  scanf("%d %d",&a,&b);
+  c=a+b;
+  printf("\n sum is %d",c);
+}*/
+
+/*#include<stdio.h>
+int main()
+{
+  int  a,b,c,d,e,f;
+  printf("\n Enter two no.s: ");
+  scanf("%d%d",&a,&b);
+  c=a+b;
+  printf("\n sum is %d",c);
+  d=a-b;
+  printf("\n sub is %d",d);
+  e=a*b;
+  printf("\n product is %d",e);
+  f=a/b;
+  printf("\n division is %d",f);
+}*/
+
+/*#include<stdio.h>
+int main()
+{
+  int a,b,c,d,e,f;
+  printf("\n Enter marks of maths ");
+  scanf("%d",&a);
+  printf("\n Enter marks of science ");
+  scanf("%d",&b);
+  printf("\n Enter marks of english ");
+  scanf("%d",&c);
+  printf("\n Enter marks of computer ");
+  scanf("%d",&d);
+  printf("\n Enter marks of Arts ");
+  scanf("%d",&e);
+  f=(a+b+c+d+e);
+  printf("\n Total marks are %d",f);
+}*/

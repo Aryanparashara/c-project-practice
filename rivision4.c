c32d2a32c084efc4effff4750560469b3593a675
Aryanparashara <aryanparashara01@gmail.com>
2025-07-19 12:01:02 +0530

    Create rivision4.c

---
+++ rivision4.c
@@ -0,0 +1,66 @@
+/*#include<stdio.h>
+int main()
+{
+  int age;
+  printf("\n Enter the age: ");
+  scanf("%d",&age);
+  if (age >=18)
+  {
+    printf("\n Eligible ");
+  }
+  else 
+  {
+    printf("\n Not eligible ");
+  }
+}*/
+
+/*#include<stdio.h>
+int main()
+{
+  int per;
+  printf("\n Enter the percentage: ");
+  scanf("%d",&per);
+  if (per>=90)
+  {
+    printf("\n 100 per. scholarship ");
+  }
+  else if ((per>=60)&&(per<=90))
+  {
+    printf("\n 60 per. scholarship ");
+  }
+  else if ((per>=40)&&(per<=60))
+  {
+    printf("\n 40 per. scholarship ");
+  }
+  else 
+  {
+    printf("\n No scholarship ");
+  }
+}*/
+
+/*#include<stdio.h>
+int main()
+{
+  float com,sales;
+  printf("\n Enter the sales ");
+  scanf("%f",&sales);
+  if (sales>=5000)
+  {
+    com=(sales*5)/100;
+    printf("\n commision will be %f",com);
+  }
+  else if ((sales>=3000)&&(sales<5000))
+  {
+    com=(sales*3)/100;
+    printf("\n commision will be %f",com);
+  }
+  else if ((sales>=1000)&&(sales<3000))
+  {
+    com=(sales*1)/100;
+    printf("\n commision wll be %f",com);
+  }
+  else 
+  {
+    printf("\n No commision ");
+  }
+}*/

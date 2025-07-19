3e87f1b1c537407666d9a4eee99a5bc7a59cc79f
Aryanparashara <aryanparashara01@gmail.com>
2025-07-19 11:59:57 +0530

    Create rivision3.c

---
+++ rivision3.c
@@ -0,0 +1,45 @@
+/*#include<stdio.h>
+int main()
+{
+  int quantity,price,cgst,sgst,billamount,totalamount;
+  printf("\n enter the quantity ");
+  scanf("%d",&quantity);
+  printf("\n enter the price ");
+  scanf("%d",&price);
+  billamount=quantity*price;
+  printf("\n bill amount is %d",billamount);
+  cgst=(billamount*9)/100;
+  printf("\n cgst is %d",cgst);
+  sgst=(billamount*9)/100;
+  printf("\n sgst is %d",sgst);
+  totalbill=(cgst+sgst+billamount);
+  printf("\n total bill is %d",totalbill);
+}*/
+
+/*#include<stdio.h>
+int main()
+{
+  int a,b,c,d,e,f,g;
+  printf("\n Central Board of Secondary Education ");
+  printf("\n                2025-26               ");
+  printf("\n______________________________________");
+  printf("\n   sub      sub      max      marks   ");
+  printf("\n   name     code    marks    obtained ");
+  printf("\n______________________________________");
+  printf("\n   101     English   100      ");
+  scanf("%d",&a);
+  printf("\n   102      Hindi    100      ");
+  scanf("%d",&b);
+  printf("\n   103      Math     100      ");
+  scanf("%d",&c);
+  printf("\n   104    Computer   100      ");
+  scanf("%d",&d);
+  printf("\n   105     Science   100      ");
+  scanf("%d",&e);
+  printf("\n   106      Arts     100      ");
+  scanf("%d",&f);
+  g=a+b+c+d+e+f;
+  printf("\n_____________________________________");
+  printf("\n   Total marks out of 600  -  %d",g);
+  printf("\n_____________________________________");
+}*/

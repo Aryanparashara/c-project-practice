fefc457145ee80f681a5833efc197064394c455f
Aryanparashara <aryanparashara01@gmail.com>
2025-07-20 12:17:44 +0530

    Create rivision6.c

---
+++ rivision6.c
@@ -0,0 +1,45 @@
+/*#include<stdio.h>
+int main()
+{
+  int a,b,c,d,e,f,g,per;
+  printf("\n CENTRAL BOARD OF SECONDARY EDUCATION ");
+  printf("\n               2025-26                ");
+  printf("\n______________________________________");
+  printf("\n   sub      sub      max      marks   ");
+  printf("\n   code     name    marks    obtained ");
+  printf("\n______________________________________");
+  printf("\n   101     English   100      ");
+  scanf("%d",&a);
+  printf("\n   102      Hindi    100      ");
+  scanf("%d",&b);
+  printf("\n   103      Math     100      ");
+  scanf(ut"%d",&c)
+  printf("\n   104     Computer  100      ");
+  scanf("%d",&d);
+  printf("\n   105     Science   100      ");
+  scanf("%d",&e);
+  printf("\n   106      Arts     100      ");
+  scanf("%d",&f);
+  g=a+b+c+d+e+d+f;
+  printf("\n_______________________________________");
+  printf("\n Total marks out of 600 - %d",f);
+  printf("\n_______________________________________");
+  per=(g*100)/600;
+  printf("\n_______________________________________");
+  if (per>90)
+  {
+    printf("\n 100 per. scholarship ");
+  }
+  else if ((per>=60)&&(per<=90))
+  {
+    printf("\n 60 per. scholarship ");
+  }
+  else if ((per>=40)&&(per<=60))
+  {
+    printf("\n 40 per. scholarship ");
+  }
+  else 
+  {
+    printf("\n No scholarship ");
+  }
+}*/

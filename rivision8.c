5d6eb5464d2b2a5efe1a661ea92689e03cd8529e
Aryanparashara <aryanparashara01@gmail.com>
2025-07-21 11:35:49 +0530

    Create rivision8.c

---
+++ rivision8.c
@@ -0,0 +1,46 @@
+/*#include<stdio.h>
+#include<stdlib.h>
+int main()
+{
+  int a,b,c,d,e,f,ch;
+  printf("\n 1.Addition ");
+  printf("\n 2.Subtraction ");
+  printf("\n 3.Product ");
+  printf("\n 4.Division ");
+  printf("\n 5.Exit ");
+  printf("\n Enter your choice: ");
+  scanf("%d",&ch);
+  switch(ch)
+  {
+    case 1:
+    printf("\n Enter 2 no.s ");
+    scanf("%d%d",&a,&b);
+    c=a+b;
+    printf("\n sum is %d",c);
+    break;
+    case 2:
+    printf("\n Enter 2 no.s ");
+    scanf("%d%d",&a,&b);
+    d=a-b;
+    printf("\n sub is %d",d);
+    break;
+    case 3:
+    printf("\n Enter 2 no.s ");
+    scanf("%d%d",&a,&b);
+    e=a*b;
+    printf("\n product is %d",e);
+    break;
+    case 4:
+    printf("\n Enter 2 no.s ");
+    scanf("%d%d",&a,&b);
+    f=a/b;
+    printf("\n division is %d",f);
+    break;
+    case 5:
+    exit(0);
+    break;
+    default:
+    printf("\n Invalid choice ");
+    printf("\n Enter 1,2,3,4,5 ");
+  }
+}*/

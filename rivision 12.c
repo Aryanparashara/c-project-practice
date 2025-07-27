d3c6582728fbb9042805810446543ac1fa08c33d
Aryanparashara <aryanparashara01@gmail.com>
2025-07-27 22:01:48 +0530

    Create rivision 12.c

---
+++ rivision 12.c
@@ -0,0 +1,58 @@
+/*#include<stdio.h>
+int main()
+{
+  int aoc,voc,si,c,s,r,p,t,h,n,choice;
+  printf("\n enter 1 for area of circle ");
+  printf("\n enter 2 for simple interest");
+  printf("\n enter 3 for volume of cone ");
+  printf("\n enter 4 for cube of any no. ");
+  printf("\n enter 5 for square of any no.");
+  printf("\n enter 6 for exit ");
+  printf("\n enter your choice ");
+  scanf("%d",&choice);
+  if (choice==1)
+  {
+    printf("\n enter the radius ");
+    scanf("%d",&r);
+    aoc=(3.14*r*r);
+    printf("\n area of circle is %d",aoc);
+  }
+  else if (choice==2)
+  {
+    printf("\n enter the principle amount ");
+    scanf("%d",&p);
+    printf("\n enter the rate ");
+    scanf("%d",&r);
+    printf("\n enter the time ");
+    scanf("%d",&t);
+    si=(p*r*t)/100;
+    printf("\n simple interest is %d",si);
+  }
+  else if (choice==3)
+  {
+    printf("\n enter the radius ");
+    scanf("%d",&r);
+    printf("\n enter the height ");
+    scanf("%d",&h);
+    voc=(3.14*r*r*h)/3;
+    printf("\n volume of cone is %d",voc);
+  }
+  else if (choice==4)
+  {
+    printf("\n enter the number ");
+    scanf("%d",&n);
+    c=(n*n*n);
+    printf("\n cube is %d",c);
+  }
+  else if (choice==5)
+  {
+    printf("\n enter the number ");
+    scanf("%d",&n);
+    s=(n*n);
+    printf("\n square is %d",s);
+  }
+  else 
+  {
+    printf("\n invalid choice ");
+  }
+}*/

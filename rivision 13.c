2c0c12e600c00eaecd1ead0a96deffae49a3d639
Aryanparashara <aryanparashara01@gmail.com>
2025-07-28 21:55:07 +0530

    Create rivision 13.c

---
+++ rivision 13.c
@@ -0,0 +1,25 @@
+/*#include<stdio.h>
+int main()
+{
+ char color;
+  printf("\n Enter the traffic signal colour (R for red,G for green,Y for yellow):        ");
+  scanf("%c",&color);
+  switch(color)
+  {
+    case 'R':
+    case 'r':
+    printf("Stop! The light is Red. \n");
+    break;
+    case 'Y':
+    case 'y':
+    printf("Wait! The light is Yellow. \n");
+    break;
+    case 'G':
+    case 'g':
+    printf("Go! The light is Green. \n");
+    break;
+    default:
+    printf("Invalid! Enter R,G,Y. \n");
+    break;
+  }
+}*/

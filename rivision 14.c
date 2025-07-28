17723f3f3f3f138f9d416f1fdddda35c815cbfde
Aryanparashara <aryanparashara01@gmail.com>
2025-07-28 21:55:59 +0530

    Create rivision 14.c

---
+++ rivision 14.c
@@ -0,0 +1,46 @@
+/*#include<stdio.h>
+#include<stdlib.h>
+int main()
+{
+  int number;
+  printf("\n Enter 1 for Monday ");
+  printf("\n Enter 2 for Tuesday ");
+  printf("\n Enter 3 for Wednesday ");
+  printf("\n Enter 4 for Thursday ");
+  printf("\n Enter 5 for Friday ");
+  printf("\n Enter 6 for Satday ");
+  printf("\n Enter 7 for Sunday ");
+  printf("\n Enter 8 for Exit ");
+  printf("\n Enter the number ");
+  scanf("%d",&number);
+  switch(number)
+  {
+    case 1:
+    printf("\n The day is monday ");
+    break;
+    case 2:
+    printf("\n The day is Tuesday ");
+    break;
+    case 3:
+    printf("\n The day is Wednesday ");
+    break;
+    case 4:
+    printf("\n The day is Thursday ");
+    break;
+    case 5:
+    printf("\n The day is Friday ");
+    break;
+    case 6:
+    printf("\n The day is Saturday ");
+    break;
+    case 7:
+    printf("\n The day is Sunday ");
+    break;
+    case 8:
+    exit(0);
+    break;
+    default:
+    printf("\n Invalid choice ");
+    printf("\n enter 1,2,3,4,5,6,7,8. ");
+  }
+}*/

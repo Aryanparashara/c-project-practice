cb2d33e96a3cc5859e5c7e8316eab81c95e054cd
Aryanparashara <aryanparashara01@gmail.com>
2025-07-29 22:16:28 +0530

    Create rivision15.c

---
+++ rivision15.c
@@ -0,0 +1,26 @@
+/*#include<stdio.h>
+int main()
+{
+  char ch;
+  printf("\n Enter the character ");
+  scanf("%c",&ch);
+  switch(ch)
+  {
+    case 'A':
+    case 'a': 
+    case 'E':
+    case 'e':
+    case 'I':
+    case 'i':
+    case 'O':
+    case 'o':
+    case 'U':
+    case 'u':
+    printf("\n It is a variable ");
+    break;
+    default:
+    printf("\n It is a constant ");
+    break;
+  }
+}*/
+    

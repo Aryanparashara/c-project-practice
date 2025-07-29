467d3164f4dd698a1596cfa117c7d1d916c526de
Aryanparashara <aryanparashara01@gmail.com>
2025-07-29 22:16:57 +0530

    Create rivision16.c

---
+++ rivision16.c
@@ -0,0 +1,23 @@
+/*#include<stdio.h>
+int main()
+{
+   int n;
+   printf(" Enter the number ");
+   scanf("%d",&n);
+   if ((n>=65)&&(n<=90))
+   {
+     printf(" %d is capital letter ",n); 
+   }
+   else if ((n>=97)&&(n<=122))
+   {
+     printf(" %d is small letter ",n);
+   }
+   else if ((n>=48)&&(n<=57))
+   {
+     printf(" %d is number key ",n);
+   }
+   else 
+   {
+     printf(" %d is an special key ",n);  
+   }
+  }*/

79d23b11c3d31138e204ac10cb712963bc1dc907
Aryanparashara <aryanparashara01@gmail.com>
2025-07-30 10:21:26 +0530

    Create rivision17.c

---
+++ rivision17.c
@@ -0,0 +1,29 @@
+/*#include<stdio.h>
+int main()
+{
+float fh,cl;
+int choice;
+printf("\n 1.convert temprature from fahrenheit to celsius. ");
+printf("\n 2.convert temprature  from celsius to fanrenheit. ");
+printf("\n Enter your choice (1,2): ");
+scanf("%d",&choice);
+if (choice==1)
+{
+printf("\n Enter temprature in fahrenheit: ");
+scanf("%f",&fh);
+cl=(fh-32)/1.8;
+printf(" Temprature in celsius: %f",cl);
+}
+else if (choice==2)
+{
+printf("\n Enter temprature in celsius: ");
+scanf("%f",&cl);
+fh=(cl*1.8)+32;
+printf(" Temprature in fahrenheit: %f",fh);
+}
+else 
+{
+printf("\n Invalid choice!!! ");
+}
+return 0;
+}*/

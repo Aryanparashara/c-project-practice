8ee95d2e7243548a2f15aa704b0718c221d0e4a9
Aryanparashara <aryanparashara01@gmail.com>
2025-07-30 22:28:03 +0530

    Create rivision18.c

---
+++ rivision18.c
@@ -0,0 +1,15 @@
+/*#include<stdio.h>
+int main()
+{
+  int n;
+  printf("\n Enter the number: ");
+  scanf("%d",&n);
+  if (n>0)
+  {
+    printf(" %d is positive number. ",n);
+  }
+  else 
+  {
+    printf(" %d is negative number. ",n);
+  }
+}*/

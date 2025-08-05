b4078210a9ac9e74da06d4202b7b90dfa2055b63
Aryanparashara <aryanparashara01@gmail.com>
2025-08-05 10:24:15 +0530

    Create rivision20.c

---
+++ rivision20.c
@@ -0,0 +1,21 @@
+/*#include<stdio.h>
+  int main()
+  {
+    int a,b;
+    printf("\n Enter two numbers ");
+    scanf("%d%d",&a,&b);
+    if (a>b)
+    {
+      goto g;
+    }
+    else 
+    {
+      goto s;
+    }
+    g:
+    printf("\n a is greater than b ");
+    goto end;
+    s:
+    printf("\n b is greater than a ");
+    end:
+  }*/

e9bddbdfe8f5c8119609a042011693e54bfba6dd
Aryanparashara <aryanparashara01@gmail.com>
2025-08-05 10:23:30 +0530

    Create rivision19.c

---
+++ rivision19.c
@@ -0,0 +1,22 @@
+/*#include<stdio.h>
+  int main()
+  {
+    int age;
+    printf(" Enter your age: ");
+    scanf("%d",&age);
+    if (age>=18)
+    {
+      goto g;
+    }
+    else
+    {
+      goto s;
+    }
+    g:
+    printf(" You are eligible. \n ");
+    goto end;
+    s:
+    printf(" You are not eligible. \n ");
+    end:
+    return 0;
+  }*/

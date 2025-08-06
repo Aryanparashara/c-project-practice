147d4a996994e74b81bb0bd2c4f7325d1ef53ce5
Aryanparashara <aryanparashara01@gmail.com>
2025-08-06 11:21:16 +0530

    Create rivision22.c

---
+++ rivision22.c
@@ -0,0 +1,15 @@
+// example of continue statement
+
+/*#include<stdio.h>
+int main()
+{
+  int i;
+  for(i=1;i<=10;i++)
+  {
+    if(i==5)
+    {
+      continue;
+    }
+    printf("\n %d",i);
+  }
+}*/

4e1d2032e08ccf035ec278c99fd8649edbc7dc44
Aryanparashara <aryanparashara01@gmail.com>
2025-08-07 11:25:37 +0530

    Create rivision24.c

---
+++ rivision24.c
@@ -0,0 +1,14 @@
+// updated table of any number: 
+
+/*#include<stdio.h>
+int main()
+{
+  int i,n,ans;
+  printf("\n Enter the number: ");
+  scanf("%d",&n);
+  for(i=1;i<=10;i++)
+  {
+    ans=n*i;
+    printf("\n %d * %d = %d ",n,i,ans);
+  }
+}*/

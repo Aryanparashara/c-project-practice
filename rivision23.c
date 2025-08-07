0e9b7254bf86ec74fe72e5f4d4fa4911c1993eac
Aryanparashara <aryanparashara01@gmail.com>
2025-08-07 11:25:04 +0530

    Create rivision23.c

---
+++ rivision23.c
@@ -0,0 +1,14 @@
+// Table of any no.
+
+/*#include<stdio.h>
+int main()
+{
+  int i,n,ans;
+  printf("\n enter the number: ");
+  scanf("%d",&n);
+  for(i=1;i<=10;i++)
+  {
+    ans=n*i;
+    printf("\n %d",ans);
+  }
+}*/

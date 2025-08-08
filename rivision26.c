ca6448c4bffcfccd26e8af9372a42fe296c8ddac
Aryanparashara <aryanparashara01@gmail.com>
2025-08-08 23:03:13 +0530

    Create rivision26.c

---
+++ rivision26.c
@@ -0,0 +1,14 @@
+// Even numbers:
+
+/*#include<stdio.h>
+int main()
+{
+  int i,sum=0;
+  printf("\n first 10 even numbers: ");
+  for(i=2;i<=20;i=i+2)
+  {
+    printf("\n %d ",i);
+    sum=sum+i;
+  }
+  printf("\n sum is %d",sum);
+}*/

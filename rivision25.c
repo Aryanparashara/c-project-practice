6b12fb040eeaccfbda7cbfa32739c2aa1bbc91f8
Aryanparashara <aryanparashara01@gmail.com>
2025-08-08 23:02:46 +0530

    Create rivision25.c

---
+++ rivision25.c
@@ -0,0 +1,14 @@
+// Natural numbers.
+
+/*#include<stdio.h>
+int main()
+{
+  int i,sum=0;
+  printf("\n first 10 natural numbers: ");
+  for(i=1;i<=10;i++)
+  {
+    printf(" \n %d ",i);
+  sum=sum+i;
+  }
+  printf("\n sum is %d",sum);
+}*/

961856e2a827f05762a1acedf2cb1ad5ac3e5245
Aryanparashara <aryanparashara01@gmail.com>
2025-08-11 11:36:22 +0530

    Create rivision27.c

---
+++ rivision27.c
@@ -0,0 +1,14 @@
+// odd numbers.
+
+/*#include<stdio.h>
+int main()
+{
+  int i,sum=0;
+  printf("\n first 10 odd numbers: ");
+  for(i=1;i<=20;i=i+2)
+  {
+    printf("\n %d ",i);
+  sum = sum+i;
+  }
+  printf("\n sum is %d",sum);
+}*/

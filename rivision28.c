35e0ea0114f5fef17a7951bc172e9b235862bc5b
Aryanparashara <aryanparashara01@gmail.com>
2025-08-11 11:36:52 +0530

    Create rivision28.c

---
+++ rivision28.c
@@ -0,0 +1,46 @@
+// do while programs 1.c
+
+/*#include<stdio.h>
+int main()
+{
+  int i=1,sum=0;
+  printf("\n first 10 natural numbers ");
+  do
+  {
+    printf("\n %d",i);
+    sum=sum+i;
+    i++;
+  }
+  while(i<=10);
+  printf("\n sum is %d",sum);
+}*/
+
+/*#include<stdio.h>
+int main()
+{
+  int i=2,sum=0;
+  printf("\n first 10 even numbers ");
+  do
+  {
+    printf("\n %d",i);
+    sum=sum+i;
+    i=i+2;
+  }
+  while(i<=20);
+  printf("\n sum is %d",sum);
+}*/
+
+/*#include<stdio.h>
+int main()
+{
+  int i=1,sum=0;
+  printf("\n first 10 odd numbers ");
+  do
+  {
+    printf("\n %d",i);
+    sum=sum+i;
+    i=i+2;
+  }
+  while(i<=20);
+  printf("\n sum is %d",sum);
+}*/

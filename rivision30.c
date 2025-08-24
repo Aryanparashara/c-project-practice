06715ac99622e0d0e71e4e83abb993ac353a0cc4
Aryanparashara <aryanparashara01@gmail.com>
2025-08-24 21:19:30 +0530

    Create rivision30.c

---
+++ rivision30.c
@@ -0,0 +1,27 @@
+/*#include<stdio.h>
+int main()
+{
+  int n,i,ans=1;
+  printf("\n Enter the number: ");
+  scanf("%d",&n);
+  for(i=1;i<=n;i++)
+  {
+    ans=ans*i;
+  }
+  printf(" factorial is %d",ans);
+}*/
+
+/*#include<stdio.h>
+int main()
+{
+  int i=1,n,ans=1;
+  printf("\n Enter the number: ");
+  scanf("%d",&n);
+  do
+  {
+    ans=ans*i;
+    i++;
+  }
+  while(i<=n);
+  printf("\n factorial is %d",ans);
+}*/

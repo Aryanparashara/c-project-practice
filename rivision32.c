27d83ccc7f5921fa14b31a8b7e0ebd0380a19ee9
Aryanparashara <aryanparashara01@gmail.com>
2025-08-25 11:32:02 +0530

    Create rivision32.c

---
+++ rivision32.c
@@ -0,0 +1,57 @@
+// while programs 2.c
+
+/*#include<stdio.h>
+int main()
+{
+  int i=1,sum=0;
+  printf("\n First 10 natural numbers ");
+  while(i<=10)
+  {
+    printf("\n %d",i);
+    sum=sum+i;
+    i++;
+  }
+  printf("\n sum of first 10 natural numbers is %d",sum);
+}*/
+
+/*#include<stdio.h>
+int main()
+{
+  int i=2,sum=0;
+  printf("\n first 10 even numbers ");
+  while(i<=20)
+  {
+    printf("\n %d",i);
+    sum=sum+i;
+    i=i+2;
+  }
+  printf("\n sum of first 10 even numbers is %d",sum);
+}*/
+
+/*#include<stdio.h>
+int main()
+{
+  int i=1,sum=0;
+  printf("\n first 10 odd nummbers ");
+  while(i<=20)
+  {
+    printf("\n %d",i);
+    sum=sum+i;
+    i=i+2;
+  }
+  printf("\n sum of first 10 odd numbers is %d",sum);
+}*/
+
+/*#include<stdio.h>
+int main()
+{
+  int i=1,ans=1,n;
+  printf("\n Enter the number: ");
+  scanf("%d",&n);
+  while(i<=n)
+  {
+    ans=ans*i;
+    i++;
+  }
+  printf("\n Factorial is %d",ans);
+}*/

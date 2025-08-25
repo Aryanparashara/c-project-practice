b926155807073b50e560c9fa581323fc4c597229
Aryanparashara <aryanparashara01@gmail.com>
2025-08-25 11:31:39 +0530

    Create rivision31.c

---
+++ rivision31.c
@@ -0,0 +1,60 @@
+// while programs 1.c
+
+/*#include<stdio.h>
+int main()
+{
+  int i=1;
+  while(i<=10)
+  {
+    printf("\n %d",i);
+    if(i==5)
+    {
+      break;
+    }
+    i++;
+  }
+  printf("\n came outside of loop i=%d",i);
+}*/
+
+/*#include<stdio.h>
+int main()
+{
+  int i=1;
+  while(i<=10)
+  {
+    printf("\n %d",i);
+    i++;
+    if(i==5)
+    {
+      continue;
+    }
+  }
+}*/
+
+/*#include<stdio.h>
+int main()
+{
+  int i=1,n,ans;
+  printf("\n Enter the number: ");
+  scanf("%d",&n);
+  while(i<=10)
+  {
+    ans=n*i;
+    printf("\n %d",ans);
+    i++;
+  }
+}*/
+
+/*#include<stdio.h>
+int main()
+{
+  int i=1,n,ans;
+  printf("\n Enter the Number: ");
+  scanf("%d",&n);
+  while(i<=10)
+  {
+    ans=n*i;
+    printf("\n %d*%d=%d",n,i,ans);
+    i++;
+  }
+}*/

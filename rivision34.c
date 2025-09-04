d4fc6d70e0ad1314e0d796d46342ec470fc1e85b
Aryanparashara <aryanparashara01@gmail.com>
2025-09-04 22:07:33 +0530

    Create rivision34.c

---
+++ rivision34.c
@@ -0,0 +1,19 @@
+/*#include<stdio.h>
+int simpleinterest(int x,int y,int z)
+{
+  int si;
+  si=(x*y*z)/100;
+  printf("\n si is %d",si);
+}
+int main()
+{
+  int p,r,t;
+  printf("\n enter the principle amount ");
+  scanf("%d",&p);
+  printf("\n enter the rate ");
+  scanf("%d",&r);
+  printf("\n enter the time ");
+  scanf("%d",&t);
+  simpleinterest(p,r,t);
+  return 0;
+  }*/

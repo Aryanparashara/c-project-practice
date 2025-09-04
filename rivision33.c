5a6a027bae67d488db61bd550eadd6b7a3d8a8e3
Aryanparashara <aryanparashara01@gmail.com>
2025-09-04 22:07:08 +0530

    Create rivision33.c

---
+++ rivision33.c
@@ -0,0 +1,27 @@
+/*#include<math.h>
+#include<stdio.h>
+int main()
+{
+  int no,p,result;
+  printf("\n enter the number ");
+  scanf("%d",&no);
+  printf("\n enter its power ");
+  scanf("%d",&p);
+  result=pow(no,p);
+  printf("\n answer is %d",result);
+}*/
+/*#include<stdio.h>
+int add(int a,int b)
+{
+  int sum=0;
+  sum=a+b;
+  printf("\n the result is %d",sum);
+}
+int main()
+{
+  int num1,num2;
+  printf("\n enter two values ");
+  scanf("%d%d",&num1,&num2);
+  add(num1,num2);
+  return 0;
+}*/

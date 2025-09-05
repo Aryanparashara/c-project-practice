1d396a127fd6e4fbf685d7bafa56b01ad93c94b7
Aryanparashara <aryanparashara01@gmail.com>
2025-09-05 12:07:12 +0530

    Create rivision35.c

---
+++ rivision35.c
@@ -0,0 +1,32 @@
+/*#include<stdio.h>
+int evenodd(int x)
+{
+  if(x%2==0)
+  printf("\n even ");
+  else 
+  printf("\n odd ");
+}
+int main()
+{
+  int x;
+  printf("\n enter the no. ");
+  scanf("%d",&x);
+  evenodd(x);
+  return 0;
+}*/
+/*#include<stdio.h>
+evenodd();
+int main()
+{
+  evenodd();
+}
+evenodd()
+{
+  int a;
+  printf("\n enter any number ");
+  scanf("%d",&a);
+  if(a%2==0)
+  printf("\n even ");
+  else 
+  printf("\n odd ");
+}*/

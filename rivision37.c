3a341ac8eef5dce8f94f10536895e3ab71352dcf
Aryanparashara <aryanparashara01@gmail.com>
2025-09-06 20:38:05 +0530

    Create rivision37.c

---
+++ rivision37.c
@@ -0,0 +1,25 @@
+/*#include<stdio.h>
+int add(int ,int);
+float average(int,int);
+int main()
+{
+  int n1,n2,sum;
+  float avg;
+  printf("\n enter 2 numbers ");
+  scanf("%d%d",&n1,&n2);
+  printf("\n Number 1 is %d",n1);
+  printf("\n Number 2 is %d",n2);
+  sum=add(n1,n2);
+  avg=average(n1,n2);
+  printf("\n sum %d",sum);
+  printf("\n average %f",avg);
+  return 0;
+}
+int add(int x,int y)
+{
+  return x+y;
+}
+float average(int x,int y)
+{
+  return((float)(x)+(float)(y))/2;
+}*/
